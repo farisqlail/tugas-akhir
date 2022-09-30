d9ff3555ee9dae1fb22071fc1eb6455e08bc80ef
farisqlail <faris.riskilail@gmail.com>
2022-09-30 15:51:11 +0700

    migration

--- database/migrations/2022_09_30_035736_pelamar.php
+++ database/migrations/2022_09_30_035736_pelamar.php
@@ -13,7 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('pelamar', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_lowongan');
+            $table->string('nama_pelamar');
+            $table->date('tanggal_lahir');
+            $table->string('tempat_lahir');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('no_telepon');
+            $table->string('jenis_kelamin');
+            $table->string('cv');
+            $table->string('ijazah');
+            $table->string('pas_foto');
+            $table->string('seleksi_satu');
+            $table->string('seleksi_dua');
+            $table->string('status_wawancara');
+            $table->string('status_dokumen');
+            $table->integer('nilai_tes');
+            $table->longText('hasil_wawancara');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +44,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('pelamar');
     }
 };
